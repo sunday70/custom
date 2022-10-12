664c4579de9388fc53e34984c0d0b7eb5d25c929
sunday70 <85828382+sunday70@users.noreply.github.com>
2022-10-12 12:52:15 +0800

    Add files via upload

---
+++ chadrc.lua
@@ -0,0 +1,37 @@
+-- First read our docs (completely) then check the example_config repo
+
+local M = {}
+
+M.options = {
+  nvChad = {
+    update_url = "https://github.com/NvChad/NvChad",
+    update_branch = "main",
+  },
+}
+
+M.ui = {
+  theme = "nord",
+}
+
+M.plugins = {
+  user = {
+    ["neovim/nvim-lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.love"
+    end,
+    },
+--    
+    ["jose-elias-alvarez/null-ls.nvim"] = {
+      after = "nvim-lspconfig",
+      config = function()
+         require "custom.null"
+      end,
+    }
+  }
+}
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
+return M

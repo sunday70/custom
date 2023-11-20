87f775fce68bb4e3d4945c2bb9a2d83915bd4416
aaa <aaa@aa.com>
2023-11-20 02:26:06 +0000

    更新了lsp支持列表

--- love.lua
+++ love.lua
@@ -3,7 +3,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "jdtls"}
+local servers = { "volar", "tsserver", "html", "cssls", "jdtls", "jsonls", "sumneko_lua", "marksman", "sqlls", "lemminx", "yamlls"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {

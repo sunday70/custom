1836f7ab1ddf7eba498722124b2bfa723e5b2144
wednesday03 <13722550+wednesday03@user.noreply.gitee.com>
2023-11-20 14:30:46 +0800

    添加配置

---
+++ README.md
@@ -0,0 +1,15 @@
+# install
+
+```bash
+
+git clone --depth 1 https://github.com/ticktechman/nvchad-custom ~/.config/nvim/lua/custom
+
+```
+
+# info
+
+This repo is based on https://github.com/NvChad/example_config.
+
+# reference
+
+- lsp name for nvim : `.local/share/nvim/lazy/nvim-lspconfig/doc/server_configurations.md`
---
+++ configs/lspconfig.lua
@@ -0,0 +1,26 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- if you just want default config for the servers then put them in a table
+local servers = { 
+  -- java, python, go, c#
+  "jdtls", "jedi_language_server", "gopls", "omnisharp",
+  -- sql, markdown, json, xml, yml
+  "sqlls", "marksman", "jsonls", "lemminx", "yamlls",
+  -- html, css, js, vue
+  "html", "cssls", "tsserver", "volar",
+  -- c, shell, toml
+  "clangd", "bashls", "taplo" 
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- 
+-- lspconfig.pyright.setup { blabla}

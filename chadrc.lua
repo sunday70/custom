53f465be4dcfaf61c6e42849bc576b03976c9c7c
wednesday03 <13722550+wednesday03@user.noreply.gitee.com>
2023-12-21 16:12:07 +0800

    edit color

--- chadrc.lua
+++ chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "everforest",
-  -- theme_toggle = { "everforest", "" },
+  theme = "nord",
+  -- theme_toggle = { "nord", "" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,

78bd7c408155cff183a78846077e1c623b29726a
wednesday03 <13722550+wednesday03@user.noreply.gitee.com>
2024-01-21 16:42:16 +0800

    update plugins

--- plugins.lua
+++ plugins.lua
@@ -98,25 +98,16 @@ local plugins = {
     },
   },
 
-  -- install without yarn or npm
+  -- markdown-preview
   {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  },
-
-  -- install with yarn or npm
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && yarn install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
+    {
+      {
+        "iamcco/markdown-preview.nvim",
+        ft = "markdown",
+        -- build = "cd app && yarn install",
+        build = ":call mkdp#util#install()",
+      },
+    },
   },
 
   -- To make a plugin not be loaded

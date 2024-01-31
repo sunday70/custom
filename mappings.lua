8369507e19086086b8300bd72416533c83622be8
wednesday03 <13722550+wednesday03@user.noreply.gitee.com>
2024-01-31 17:40:21 +0800

    update mappings

--- mappings.lua
+++ mappings.lua
@@ -324,73 +324,73 @@ M.general = {
 --   },
 -- }
 --
--- M.nvterm = {
---   plugin = true,
---
---   t = {
---     -- toggle in terminal mode
---     ["<A-i>"] = {
---       function()
---         require("nvterm.terminal").toggle "float"
---       end,
---       "toggle floating term",
---     },
---
---     ["<A-h>"] = {
---       function()
---         require("nvterm.terminal").toggle "horizontal"
---       end,
---       "toggle horizontal term",
---     },
---
---     ["<A-v>"] = {
---       function()
---         require("nvterm.terminal").toggle "vertical"
---       end,
---       "toggle vertical term",
---     },
---   },
---
---   n = {
---     -- toggle in normal mode
---     ["<A-i>"] = {
---       function()
---         require("nvterm.terminal").toggle "float"
---       end,
---       "toggle floating term",
---     },
---
---     ["<A-h>"] = {
---       function()
---         require("nvterm.terminal").toggle "horizontal"
---       end,
---       "toggle horizontal term",
---     },
---
---     ["<A-v>"] = {
---       function()
---         require("nvterm.terminal").toggle "vertical"
---       end,
---       "toggle vertical term",
---     },
---
---     -- new
---
---     ["<leader>h"] = {
---       function()
---         require("nvterm.terminal").new "horizontal"
---       end,
---       "new horizontal term",
---     },
---
---     ["<leader>v"] = {
---       function()
---         require("nvterm.terminal").new "vertical"
---       end,
---       "new vertical term",
---     },
---   },
--- }
+M.nvterm = {
+  plugin = true,
+
+  t = {
+    -- toggle in terminal mode
+    ["<A-i>"] = {
+      function()
+        require("nvterm.terminal").toggle "float"
+      end,
+      "toggle floating term",
+    },
+
+    ["<A-h>"] = {
+      function()
+        require("nvterm.terminal").toggle "horizontal"
+      end,
+      "toggle horizontal term",
+    },
+
+    ["<A-v>"] = {
+      function()
+        require("nvterm.terminal").toggle "vertical"
+      end,
+      "toggle vertical term",
+    },
+  },
+
+  n = {
+    -- toggle in normal mode
+    ["<A-i>"] = {
+      function()
+        require("nvterm.terminal").toggle "float"
+      end,
+      "toggle floating term",
+    },
+
+    ["<A-h>"] = {
+      function()
+        require("nvterm.terminal").toggle "horizontal"
+      end,
+      "toggle horizontal term",
+    },
+
+    ["<A-v>"] = {
+      function()
+        require("nvterm.terminal").toggle "vertical"
+      end,
+      "toggle vertical term",
+    },
+
+    -- new
+
+    ["<leader>h"] = {
+      function()
+        require("nvterm.terminal").new "horizontal"
+      end,
+      "new horizontal term",
+    },
+
+    ["<leader>v"] = {
+      function()
+        require("nvterm.terminal").new "vertical"
+      end,
+      "new vertical term",
+    },
+  },
+}
 --
 -- M.whichkey = {
 --   plugin = true,

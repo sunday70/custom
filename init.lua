f1a3770bfc5756602a0f42f53239bf7173fcfcd8
wednesday03 <13722550+wednesday03@user.noreply.gitee.com>
2024-02-20 10:18:41 +0800

    update init.lua

--- init.lua
+++ init.lua
@@ -73,8 +73,8 @@ cmd [[
 ]]
 
 autocmd("FileType", {
-  -- pattern = { "vue", "typescript", "javascript", "html", "css", "c", "cpp", "lua", "sh", "go" },
-  pattern = { "c", "cpp", "lua", "sh", "go" }, -- 自动格式化代码
+  pattern = { "vue", "typescript", "javascript", "html", "css", "c", "cpp", "lua", "sh", "go" },
+  -- pattern = { "c", "cpp", "lua", "sh", "go" }, -- 自动格式化代码
   callback = function()
     autocmd("BufWritePre", {
       callback = function()
